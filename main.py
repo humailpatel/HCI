8f404cce59bb1ec21b57138266e65e621bc7f258
Humail Patel <humailxpatel@gmail.com>
2023-11-23 23:17:48 +0000

    change main

--- main.py
+++ main.py
@@ -1,199 +1,108 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+import sys
 import ast
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import simpledialog
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def parse_python_file(file_path):
-    with open(file_path, "r") as source:
-        code = source.read()
-        return ast.parse(code)
-    
-class VariableFlowAnalyzer(ast.NodeVisitor):
-    def __init__(self, variable_name):
+def get_defined_functions(file_path):
+    with open(file_path, 'r') as file:
+        code = file.read()
+        tree = ast.parse(code)
+        return [node.name for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)]
+
+class RuntimeFlowTracer:
+    def __init__(self, variable_name, defined_functions):
         self.variable_name = variable_name
+        self.defined_functions = defined_functions
         self.flow = []
-        self.current_function = None
-        self.occurrence_counter = 0
+        self.last_func = None
+        self.last_lineno = None
+        self.current_value = None
+        self.line_group = []
 
-    def visit_FunctionDef(self, node):
-        self.current_function = node.name
-        self.generic_visit(node)
-        self.current_function = None
+    def trace_calls(self, frame, event, arg):
+        lineno = frame.f_lineno
+        func_name = frame.f_code.co_name
 
-    def process_variable_occurrence(self, lineno):
-        # Add a placeholder (e.g., None) for the conditional type
-        unique_occurrence = (self.current_function, lineno, self.occurrence_counter, None)
-        self.flow.append(unique_occurrence)
-        self.occurrence_counter += 1
+        if func_name not in self.defined_functions:
+            return self.trace_calls  # Skip functions not defined in the file
 
-    def visit_Assign(self, node):
-        if isinstance(node.targets[0], ast.Name) and node.targets[0].id == self.variable_name:
-            self.process_variable_occurrence(node.lineno)
-        self.generic_visit(node)
+        if self.last_func != func_name:
+            # New function call
+            if self.line_group:
+                self.flow.append((self.last_func, self.line_group, self.current_value))
+                self.line_group = []
+            self.last_func = func_name
+            self.current_value = None
 
-    def visit_Call(self, node):
-        for arg in node.args:
-            if isinstance(arg, ast.Name) and arg.id == self.variable_name:
-                self.process_variable_occurrence(node.lineno)
-        self.generic_visit(node)
+        if lineno not in self.line_group:
+            self.line_group.append(lineno)
 
-    def visit_If(self, node):
-    # Process the 'if' part
-        self.process_conditional_occurrence(node.lineno, 'if')
-        self.generic_visit(node.test)  # Visit the test expression of the 'if' statement
+        self.last_lineno = lineno
 
-        # Process the body of the 'if'
-        for n in node.body:
-            self.visit(n)  # Visit each node in the body of the 'if'
+        if event == 'line' and self.variable_name in frame.f_locals:
+            current_value = frame.f_locals[self.variable_name]
+            self.current_value = current_value
 
-        # Process 'elif' and 'else' parts
-        for orelse in node.orelse:
-            if isinstance(orelse, ast.If):
-                self.process_conditional_occurrence(orelse.lineno, 'elif')
-                self.generic_visit(orelse.test)
-                for n in orelse.body:
-                    self.visit(n)
-            elif isinstance(orelse, ast.Expr):
-                self.visit(orelse)
-            else:
-                # Handle other types of nodes in the 'else' part
-                for n in getattr(orelse, 'body', []):
-                    self.visit(n)
+        return self.trace_calls
 
-    def process_conditional_occurrence(self, lineno, conditional_type):
-        unique_occurrence = (self.current_function, lineno, self.occurrence_counter, conditional_type)
-        self.flow.append(unique_occurrence)
-        self.occurrence_counter += 1
-    
+    def finalize_flow(self):
+        if self.line_group:
+            self.flow.append((self.last_func, self.line_group, self.current_value))
 
-def get_current_function(lineno, code):
-    """Parse the code and find the function name for the given line number."""
-    tree = ast.parse(code)
-    for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef):
-            start_line = node.lineno
-            end_line = node.end_lineno if hasattr(node, 'end_lineno') else start_line
-            if start_line <= lineno <= end_line:
-                return node.name
-    return "Global Scope"
 
-def visualize_flow(flow, current_function):
+
+def start_tracing(variable_name):
+    tracer = RuntimeFlowTracer(variable_name)
+    sys.settrace(tracer.trace_calls)
+    return tracer
+
+def stop_tracing(tracer):
+    tracer.finalize_flow()
+    sys.settrace(None)
+    return tracer.flow
+
+def visualize_flow(flow):
     G = nx.DiGraph()
 
-    # Function map to track occurrences
-    func_map = {}
-    for func, lineno, _, _ in flow:
-        if func not in func_map:
-            func_map[func] = []
-        func_map[func].append(lineno)
-
-    print(func_map)
-    # Create node label map for the graph
-    node_label_map = {func: f"{func}\nLines: {', '.join(map(str, lines))}" for func, lines in func_map.items()}
-
-    # Add nodes to the graph
-    for func, node_label in node_label_map.items():
-        G.add_node(node_label)
-
-    # Add edges between nodes based on the flow
+    # Process the flow data
     prev_node_label = None
-    for func, lineno, _, conditional_type in flow:
-        print(func, lineno, _, conditional_type)
-        node_label = node_label_map[func]
+    for func, line_group, value in flow:
+        line_range = f"{line_group[0]}-{line_group[-1]}" if len(line_group) > 1 else str(line_group[0])
+        node_label = f"{func} (Lines {line_range})\nVar Value: {value}"
 
-        # Differentiate edges for conditional flows
-        if conditional_type in ['if', 'elif', 'else']:
-            edge_style = 'dotted'
-        else:
-            edge_style = 'solid'
-
-        if prev_node_label is not None and prev_node_label != node_label:
-            G.add_edge(prev_node_label, node_label, style=edge_style)
-        prev_node_label = node_label
-
-    # Determine the colors for each node
-    node_colors = [get_node_color(func, current_function, func_map) for func, _ in node_label_map.items()]
+        if node_label not in G.nodes:
+            G.add_node(node_label)
+            if prev_node_label:
+                G.add_edge(prev_node_label, node_label)
+            prev_node_label = node_label
 
     # Layout and drawing
     pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-    nx.draw_networkx_labels(G, pos)
-
-    # Draw edges with respective styles
-    for u, v, attr in G.edges(data=True):
-        style = attr.get('style', 'solid')
-        nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], style=style, arrows=True)
-
+    nx.draw_networkx(G, pos)
     plt.show()
 
-
-def get_node_color(func, current_function, func_map):
-    if func == current_function:
-        return 'green'  # Current function
-    elif list(func_map).index(func) < list(func_map).index(current_function):
-        return 'red'  # Previous functions
-    else:
-        return 'blue'  # Future functions
-
-
-def analyze_highlighted_variable():
-    try:
-        if not text.tag_ranges(tk.SEL):
-            print("Please select a variable name in the text.")
-            return
-        print("OK")
-        start_index = text.index(tk.SEL_FIRST)
-        print(start_index)
-        end_index = text.index(tk.SEL_LAST)
-        print(end_index)
-        variable_name = text.get(start_index, end_index)
-        print(variable_name)
-        lineno = int(start_index.split('.')[0])
-
-        # Determine the current function
-        current_function = get_current_function(lineno, code)
-
-        # Check if the selected text corresponds to a valid variable name
-        if not is_valid_variable_name(variable_name):
-            print(f"'{variable_name}' is not a valid variable name.")
-            return
-
-        analyzer = VariableFlowAnalyzer(variable_name)
-        analyzer.visit(ast.parse(code))
-        visualize_flow(analyzer.flow, current_function)
-    except Exception as e:
-        print("Error:", e)
-
-def is_valid_variable_name(variable_name):
-    try:
-        # Attempt to parse the variable name as an ast.Name
-        ast.parse(variable_name, mode='eval')
-        return True
-    except SyntaxError:
-        return False
-
-
-def select_variable_and_visualize(file_path):
-    global text, code
-    root = tk.Tk()
-    text = tk.Text(root)
-    text.pack(expand=True, fill="both")
-
-    analyze_button = tk.Button(root, text="Test Button", command=analyze_highlighted_variable)
-    analyze_button.pack()
-
+def execute_and_trace(file_path, variable_name, tracer):
     with open(file_path, 'r') as file:
         code = file.read()
-        text.insert('1.0', code)
-
-    root.mainloop()
+        exec_globals = {}
+        exec_globals['__name__'] = '__main__'  # Set __name__ to emulate a main execution environment
+        sys.settrace(tracer.trace_calls)  # Set the trace function
+        exec(code, exec_globals)  # Execute the code
+        sys.settrace(None)  # Stop tracing after execution
 
 def main():
     file_path = filedialog.askopenfilename()
     if file_path:
-        select_variable_and_visualize(file_path)
+        variable_name = simpledialog.askstring("Input", "Enter the variable name to trace:")
+        defined_functions = get_defined_functions(file_path)
+        if variable_name:
+            tracer = RuntimeFlowTracer(variable_name, defined_functions)
+            execute_and_trace(file_path, variable_name, tracer)
+            flow = stop_tracing(tracer)
+            visualize_flow(flow)
 
 if __name__ == "__main__":
     main()
