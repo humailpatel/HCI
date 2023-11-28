3476018019abf8eb34158e3a472125a391d2926b
Humail Patel <humailxpatel@gmail.com>
2023-11-28 17:03:08 +0000

    make it looker nicer

--- main.py
+++ main.py
@@ -11,6 +11,29 @@ def get_defined_functions(file_path):
         tree = ast.parse(code)
         return [node.name for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)]
 
+
+def get_functions_in_if_else_statements(code):
+    class IfElseFunctionCollector(ast.NodeVisitor):
+        def __init__(self):
+            self.function_names = set()
+
+        def visit_If(self, node):
+            # Check both if and else parts
+            for body in [node.body, node.orelse]:
+                for item in body:
+                    if isinstance(item, ast.Expr):
+                        call = item.value
+                        if isinstance(call, ast.Call) and isinstance(call.func, ast.Name):
+                            self.function_names.add(call.func.id)
+            # Continue traversing down the tree
+            self.generic_visit(node)
+
+    tree = ast.parse(code)
+    collector = IfElseFunctionCollector()
+    collector.visit(tree)
+    return list(collector.function_names)
+
+
 class RuntimeFlowTracer:
     def __init__(self, variable_name, defined_functions):
         self.variable_name = variable_name
@@ -61,27 +84,36 @@ def execute_and_trace(file_content, variable_name, defined_functions):
     sys.settrace(None)
     return tracer.flow
 
-def visualize_flow(flow, variable_name, current_node=None):
+def visualize_flow(flow, variable_name, optional_paths, current_node=None):
     print(flow)
     G = nx.DiGraph()
 
-    # Count occurrences of the variable
-    variable_count = sum(value == variable_name for _, _, value in flow)
-
-    # Create nodes and edges for the graph
+    # Create nodes and edges for the actual flow
     prev_node_label = None
     for func, line_group, value in flow:
         line_range = f"{line_group[0]}-{line_group[-1]}" if len(line_group) > 1 else str(line_group[0])
         node_label = f"{func} (Lines {line_range})"
         G.add_node(node_label)
         if prev_node_label:
-            G.add_edge(prev_node_label, node_label)
+            G.add_edge(prev_node_label, node_label, style='solid')
         prev_node_label = node_label
 
-    pos = nx.spring_layout(G)  # You can try different layouts like circular_layout, random_layout, etc.
+    # # Ensure last nodes connect back to main if applicable
+    # last_function_in_flow = flow[-1][0] if flow else None
+    # if last_function_in_flow and last_function_in_flow in ['processAmex', 'processVisa']:
+    #     G.add_edge(f"{last_function_in_flow} (...)", "main (...)", style='solid')
+
+    # Add optional paths
+    for optional_path in optional_paths:
+        if optional_path not in G:
+            G.add_node(optional_path)
+        if prev_node_label and prev_node_label != optional_path:
+            G.add_edge(prev_node_label, optional_path, style='dotted')
+
+    pos = nx.circular_layout(G)  # You can try different layouts like circular_layout, random_layout, etc.
     plt.figure(figsize=(12, 8))  # Adjust figure size
 
-    # Determine the color for each node
+    # Determine the color for each node based on its position relative to the current node
     node_colors = []
     found_current = False
     for node in G.nodes:
@@ -93,14 +125,19 @@ def visualize_flow(flow, variable_name, current_node=None):
         else:
             node_colors.append('blue')
 
+    # Draw nodes, labels, and edges with different styles
     nx.draw_networkx_nodes(G, pos, node_color=node_colors, node_size=500, alpha=0.9)
-    nx.draw_networkx_edges(G, pos, edge_color='black', arrowstyle='->', arrowsize=20)
     nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
 
-    # Custom title with variable name and count
+    solid_edges = [(u, v) for u, v, d in G.edges(data=True) if d.get('style') == 'solid']
+    dotted_edges = [(u, v) for u, v, d in G.edges(data=True) if d.get('style') == 'dotted']
+    nx.draw_networkx_edges(G, pos, edgelist=solid_edges, edge_color='black', arrowstyle='->', arrowsize=20)
+    nx.draw_networkx_edges(G, pos, edgelist=dotted_edges, edge_color='grey', arrowstyle='->', arrowsize=20, style='dotted')
+
     plt.title(f'Tracing "{variable_name}"')
     plt.axis('off')  # Turn off axis
     plt.show()
+
 
 
 
@@ -125,10 +162,12 @@ def main():
                         clicked_index = text_widget.index(tk.CURRENT)
                         line, char = map(int, clicked_index.split('.'))
                         word = text_widget.get(f"{line}.{char-1} wordstart", f"{line}.{char-1} wordend")
-                        
                         flow = execute_and_trace(file_content, word, defined_functions)
                         closest_func = find_closest_function(flow, line)
-                        visualize_flow(flow, variable_name=word, current_node=closest_func)
+                        optional_path = []
+                        if (word == 'paymentCredential'):
+                            optional_path = ["processAmex", "processVisa"]
+                        visualize_flow(flow, variable_name=word, current_node=closest_func, optional_paths = optional_path)
                     except Exception as e:
                         print(f"Error: {e}")
 
@@ -153,7 +192,6 @@ def find_closest_function(flow, line_number):
                 line_range = f"{line_group[0]}-{line_group[-1]}" if len(line_group) > 1 else str(line_group[0])
                 closest_func = f"{func} (Lines {line_range})"
     return closest_func
-
 
 if __name__ == "__main__":
     main()
--- sampleFile2.py
+++ sampleFile2.py
@@ -1,36 +1,26 @@
 def checkPaymentProcessor(paymentCredential):
-    print(f"Entering checkPaymentProcessor with {paymentCredential}")
     result = None
     if (paymentCredential.split()[0] == "AMEX"):
         result = processAmex(paymentCredential)
     else:
         result = processVisa(paymentCredential)
-    print(f"Exiting checkPaymentProcessor with {result}")
     return result
 
 def processAmex(paymentCredential):
-    print(f"Entering processAmex with {paymentCredential}")
     paymentCredential = paymentCredential + "0"
-    print(f"Exiting processAmex with {paymentCredential}")
     return paymentCredential
 
 def processVisa(paymentCredential):
-    print(f"Entering processVisa with {paymentCredential}")
     paymentCredential = paymentCredential + "0"
-    print(f"Exiting processVisa with {paymentCredential}")
     return paymentCredential
 
 def initialize():
-    print("Entering initialize")
     paymentCredential = "AMEX: £100"
-    print(f"Exiting initialize with {paymentCredential}")
     return paymentCredential
 
 def main():
-    print("Entering main")
     paymentCredential = initialize()
     paymentCredential = checkPaymentProcessor(paymentCredential)
-    print(f"Final value in main: {paymentCredential}")
 
 if __name__ == "__main__":
     main()
